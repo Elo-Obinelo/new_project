fc193d581927b275531ca9f4ddc41de6f05152eb
elo <elo.obinelo@gmail.com>
2024-10-13 12:53:36 +0100

    Initial Commit

---
+++ fetch.py
@@ -0,0 +1,121 @@
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime, timedelta
+import sqlite3
+
+date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+cookies = {
+    'epex-data-use-agreed': '1',
+    'cookie-agreed-version': '1.0.0',
+    'cookie-agreed-categories': '["functional"]',
+    'cookie-agreed': '2',
+}
+
+request_headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'epex-data-use-agreed=1; cookie-agreed-version=1.0.0; cookie-agreed-categories=["functional"]; cookie-agreed=2',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+areas = ['FR','BE', 'DE', 'NL']
+
+
+res1_0 =  BeautifulSoup("<td>res1</td>", "html.parser")
+res2_0 =  BeautifulSoup("<td>res2</td>", "html.parser")
+res3_0 =  BeautifulSoup("<td>res3</td>", "html.parser")
+
+def daily_fetch(params):
+    response = requests.get('https://www.epexspot.com/en/market-data', params=params, cookies=cookies, headers= request_headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    a = soup.find(class_="table-container").find("ul").find_all("li")
+    b = soup.find(class_="table-container").find("tbody").find_all("tr")
+    l = list(zip(a,b))
+
+    for index, i in enumerate(l):
+        if "child" in i[0]['class']:
+            l[index] += (res1_0,)
+        elif "lvl-1" in i[0]['class']:
+            l[index] += (res2_0,)
+        elif "lvl-2" in i[0]['class']:
+            l[index] += (res3_0,)
+
+    for item in (body := [[g.text.strip().replace("\n", "//") for g in i] for i in l]):
+        item[1] = item[1].split("//")
+
+    headers = [i.text.strip() for i in soup.find(class_="table-container").find("thead").find_all("th")]
+    headers.append('Resolution')
+
+    return headers, body, soup
+
+
+def create_database(headers, body, area):
+    conn = sqlite3.connect("new_project/stash1.db")
+    cursor = conn.cursor()
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS {area} (
+            Date TEXT,
+            Time TEXT,
+            "{headers[1]}" INTEGER,
+            "{headers[2]}" INTEGER,
+            "{headers[3]}" INTEGER,
+            "{headers[4]}" INTEGER,
+            "{headers[5]}" INTEGER,
+            "{headers[6]}" INTEGER,
+            "{headers[7]}" INTEGER,
+            "{headers[8]}" INTEGER,
+            "{headers[9]}" INTEGER,
+            "{headers[10]}" INTEGER,
+            "{headers[11]}" INTEGER,
+            PRIMARY KEY(Date, Time)
+            
+        )
+    ''')
+
+    for time, values, resolution in body:
+        cursor.execute(f"INSERT OR IGNORE INTO {area} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (date, time, *values, resolution))
+    
+    conn.commit()
+    conn.close()
+
+def verify_table(soup):
+    metadata = soup.find(class_='table-container').find('h2').text.replace("\n", "").replace(" ", "").split(">")
+    print(metadata)
+    try:
+        assert metadata[0] == 'Continuous'
+        assert metadata[2] == area
+        assert datetime.strptime(metadata[3], '%d%B%Y').strftime("%Y-%m-%d") == date
+    except AssertionError as e:
+        print(e)
+        override = input("Override Error?: ")
+
+
+if __name__ == "__main__":
+    for area in areas:
+        params = {
+            'market_area': area,
+            'auction': '',
+            'trading_date': '',
+            'delivery_date': date,
+            'underlying_year': '',
+            'modality': 'Continuous',
+            'sub_modality': '',
+            'technology': '',
+            'data_mode': 'table',
+            'period': '',
+            'production_period': '',
+            'product': '15',
+        }
+        head, body, soup = daily_fetch(params)
+        verify_table(soup)
+        create_database(head, body, area)
---
+++ sandbox.py
@@ -0,0 +1,145 @@
+from dash import Dash, html, dcc, Input, Output, State, clientside_callback
+import dash_bootstrap_components as dbc
+from datetime import date, timedelta
+
+import pandas as pd
+import sqlite3
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME], prevent_initial_callbacks= False)
+# conn = sqlite3.Connection("new_project/stash1.db")
+# cursor = conn.cursor()
+
+
+market_picker = html.Div([
+    dcc.RadioItems(
+        id='button-group',
+        options=[
+            {'label': 'BE', 'value': 'BE'},
+            {'label': 'DE', 'value': 'DE'},
+            {'label': 'FR', 'value': 'FR'},
+            {'label': 'NL', 'value': 'NL'}
+        ],
+        value='BE',
+        labelStyle={'display': 'inline-block', 'margin-right': '10px'}
+    ),
+    html.Div(id='output')
+])
+
+slider = html.Div([
+    html.Div("Interval(min)"),
+    dcc.Slider(1, 3, 1,
+               value=1,
+               id='my-slider',
+               marks= {
+                   "1": "60",
+                   "2": "30",
+                   "3": "15"
+               }
+    )
+])
+
+datepicker = html.Div([
+    dcc.DatePickerSingle(
+        id='my-date-picker-range',
+        min_date_allowed=date(2024, 9, 24),
+        max_date_allowed=date.today(),
+        initial_visible_month=date(2024, 9, 26),
+        date = date.today() - timedelta(days=2)
+    ),
+
+])
+
+offcanvas = html.Div(
+    [
+        dbc.NavbarToggler(id="open-offcanvas", n_clicks=0),
+        dbc.Offcanvas(
+            [
+            html.Div(style={'height': '100px'}),
+            slider,
+            html.Div(style={'height': '100px'}),
+            datepicker,
+            html.Div(style={'height': '100px'}),
+            market_picker
+            ],
+            id="offcanvas",
+            title="Menu",
+            is_open=False,
+        ),
+    ]
+)
+
+color_mode_switch =  html.Span(
+    [
+        dbc.Label(className="fa fa-moon", html_for="switch"),
+        dbc.Switch( id="switch", value=True, className="d-inline-block ms-1", persistence=True),
+        dbc.Label(className="fa fa-sun", html_for="switch"),
+    ]
+)
+
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col([offcanvas], width= 1),
+                dbc.Col(width= 10),
+                dbc.Col([color_mode_switch], width= 1)
+            ]
+        ),
+        html.Div(id= "my-table")
+    ],
+    className="p-4",
+    id="page-content",
+)
+
+clientside_callback(
+    """
+    (switchOn) => {
+       document.documentElement.setAttribute("data-bs-theme", switchOn ? "light" : "dark"); 
+       return window.dash_clientside.no_update
+    }
+    """,
+    Output("switch", "id"),
+    Input("switch", "value"),
+)
+
+@app.callback(
+    Output("offcanvas", "is_open"),
+    Input("open-offcanvas", "n_clicks"),
+    [State("offcanvas", "is_open")],
+)
+def toggle_offcanvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
+
+@app.callback(
+    Output('my-table', 'children'),
+    Input('my-date-picker-range', 'date'),
+    Input('button-group', 'value'),
+    Input('my-slider', 'value'))
+def update_output(date_now, market_area, resolution):
+    if date_now is not None:
+        date_object = date.fromisoformat(date_now)
+        date_string = date_object.strftime('%Y-%m-%d')
+        if resolution == 1:
+            res = 'res1'
+        elif resolution == 2:
+            res = 'res2'
+        else:
+            res = 'res3'
+
+        conn = sqlite3.connect("new_project/stash1.db")
+        cursor = conn.cursor()
+        cursor.execute(f'''
+            SELECT * FROM {market_area} WHERE  Date == '{date_string}' AND Resolution = '{res}'
+        ''')
+        headers = [i[0] for i in cursor.description]
+        df = pd.DataFrame.from_records(cursor.fetchall(), columns=headers).iloc[:,1:-1]
+        table = dbc.Table(id= 'my_table').from_dataframe(df, striped=True, bordered=True, hover=True)
+        cursor.close()
+        conn.close()
+        return table
+
+
+if __name__ == "__main__":
+    app.run_server()
